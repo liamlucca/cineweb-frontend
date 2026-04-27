94c8558851e326d55204c6ad7bc0cb430053102b
Liam <39639266+liamlucca@users.noreply.github.com>
2026-04-27 20:59:14 -0300

    estructura básica

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>Bienvenido a CineWeb</h1>
+      <p>Las mejores películas, subidas por la comunidad.</p>
+    </div>
+  )
+}
+
+export default HomePage
